ec7868d31bbc58d0dd614c62420f162e4dea06ca
Manthan Ankolekar <manthan.ank46@gmail.com>
2025-01-26 19:51:30 +0530

    chore: Update package.json and index.js to enhance project description and add security middleware

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-// index.js
-const express = require('express');
-const bodyParser = require('body-parser');
-const todoRoutes = require('./routes/todoRoutes');
-const errorMiddleware = require('./middleware/errorMiddleware');
+const express = require("express");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const todoRoutes = require("./routes/todoRoutes");
+const errorMiddleware = require("./middleware/errorMiddleware");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 // Middleware
 app.use(bodyParser.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use(cors());
 
 // Routes
-app.use('/todos', todoRoutes);
+app.use("/todos", todoRoutes);
 
 // Error middleware
 app.use(errorMiddleware);
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
